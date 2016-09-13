a23b231239f0b6de30e9e41858e4511396c5bcae
Gezze <stefan.gezelius@gmail.com>
2016-09-13 11:16:16 +0200

    Add project files.

---
+++ ConsoleApplication6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+           string input= Console.ReadLine();
+            if (input == "y")
+            {
+                Console.WriteLine("es");
+            }
+            else if (input == "n")
+            {
+                Console.WriteLine("No");
+                Console.WriteLine("jkljkljl");
+            }
+            else
+                Console.WriteLine("Else");
+
+        }
+    }
+}
